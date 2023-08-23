338fd0a180afbdeee8ecf5c262efea8aa495975e
evdrive40mil <142002838+evdrive40mil@users.noreply.github.com>
2023-08-23 15:34:32 +0300

    Update pay_for_backlink_raw_transaction.ps1

--- powershell/pay_for_backlink_raw_transaction.ps1
+++ powershell/pay_for_backlink_raw_transaction.ps1
@@ -1,24 +1,34 @@
-﻿cd $pwd
+cd $pwd
 1..10 | % { write "" }
 
-echo "Pay for link publishing"
+echo "Create Pay for link publishing transaction"
 
-$publisher_page_link = Read-Host "enter publisher page link ex(summerblog.ws)"
-$publisher_day_price = Read-Host "enter publisher price per day for placing backlink"
-$publisher_wallet_address = Read-Host "enter publisher wallet address ex(MJbvkR6oUS1rBh2abf8grm6F9X7yv3Y3Jr)"
+$publisher_day_price = Read-Host "enter publisher price per day for placing backlink, found in publisher tx "
+$publisher_wallet_address = Read-Host "enter publisher wallet address ex(MJbvkR6oUS1rBh2abf8grm6F9X7yv3Y3Jr) found in publisher tx "
 $advertiser_page_link = Read-Host "enter advertiser back link ex(boating.info)"
+
+$data_string = [System.String]::Concat("adv:",$advertiser_page_link)
+echo $data_string
+
+while($data_string.length -gt 79)
+{
+    echo $data_string
+    echo ($data_string.length -gt 79)
+    write-host "data string is above 80 char max size, reduce url length"
+    $advertiser_page_link = Read-Host "enter advertiser back link ex(boating.info)"
+
+    $data_string = [System.String]::Concat("adv:",$advertiser_page_link)
+
+}
+
+
 $days_to_publish = Read-Host "enter days to keep link published"
 $tx_vout = Read-Host "enter transaction id to spend from ex(81a11f6801c661f0e601e13c33caf6c71a86f90a06594bf7680a7b51d04e5527)"
 $tx_vout_count = Read-Host "enter spend from transaction id vout number ex(0)"
 
 $total_to_pay = [decimal]$days_to_publish * [decimal]$publisher_day_price
 
-
-
-$data_string = [System.String]::Concat("publish_on:",$publisher_page_link," ","days:",$days_to_publish," ",$advertiser_page_link)
-echo $data_string
 echo "hex encoded data for transaction"
-
 
 $char_array = $data_string.ToCharArray();
 $hex_data = ""
