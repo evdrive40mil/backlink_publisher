37d64f72bf10b61b3ec3228331a5cda5f1c0da40
evdrive40mil <142002838+evdrive40mil@users.noreply.github.com>
2023-08-23 14:44:44 +0300

    Update create_raw_transaction_publisher_site_pricing.ps1

--- powershell/create_raw_transaction_publisher_site_pricing.ps1
+++ powershell/create_raw_transaction_publisher_site_pricing.ps1
@@ -5,14 +5,30 @@ echo "Set pricing for backlinks on publisher site page"
 
 $publisher_page_link = Read-Host "enter publisher page link ex(summerblog.ws)"
 $publisher_day_price = Read-Host "enter publisher price per day for placing backlink"
+$data_string = [System.String]::Concat("url:",$publisher_page_link," price:",$publisher_day_price)
+
+while($data_string.length -gt 79)
+{
+    echo $data_string
+    echo ($data_string.length -gt 79)
+    write-host "data string is above 80 char max size, reduce url length"
+    $publisher_page_link = Read-Host "enter publisher page link ex(summerblog.ws)"
+    $publisher_day_price = Read-Host "enter publisher price per day for placing backlink"
+
+    $data_string = [System.String]::Concat("url:",$publisher_page_link," price:",$publisher_day_price)
+    
+
+}
+
+
+
+
 $publisher_wallet_address = Read-Host "enter publisher wallet address ex(MJbvkR6oUS1rBh2abf8grm6F9X7yv3Y3Jr)"
 $tx_vout = Read-Host "enter transaction id to spend from ex(81a11f6801c661f0e601e13c33caf6c71a86f90a06594bf7680a7b51d04e5527)"
 $tx_vout_count = Read-Host "enter spend from transaction id vout number ex(0)"
 
 
 
-$data_string = [System.String]::Concat("publisher_page:",$publisher_page_link,":",$publisher_day_price,":",$publisher_wallet_address)
-echo $data_string
 echo "hex encoded data for transaction"
 
 
