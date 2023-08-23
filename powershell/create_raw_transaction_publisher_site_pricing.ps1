ec2a20619279323c6a9a6c9d6812bd39b6da3504
evdrive40mil <142002838+evdrive40mil@users.noreply.github.com>
2023-08-23 14:47:34 +0300

    Update create_raw_transaction_publisher_site_pricing.ps1

--- powershell/create_raw_transaction_publisher_site_pricing.ps1
+++ powershell/create_raw_transaction_publisher_site_pricing.ps1
@@ -10,27 +10,17 @@ $data_string = [System.String]::Concat("url:",$publisher_page_link," price:",$pu
 while($data_string.length -gt 79)
 {
     echo $data_string
-    echo ($data_string.length -gt 79)
     write-host "data string is above 80 char max size, reduce url length"
     $publisher_page_link = Read-Host "enter publisher page link ex(summerblog.ws)"
     $publisher_day_price = Read-Host "enter publisher price per day for placing backlink"
-
     $data_string = [System.String]::Concat("url:",$publisher_page_link," price:",$publisher_day_price)
-    
-
 }
-
-
-
 
 $publisher_wallet_address = Read-Host "enter publisher wallet address ex(MJbvkR6oUS1rBh2abf8grm6F9X7yv3Y3Jr)"
 $tx_vout = Read-Host "enter transaction id to spend from ex(81a11f6801c661f0e601e13c33caf6c71a86f90a06594bf7680a7b51d04e5527)"
 $tx_vout_count = Read-Host "enter spend from transaction id vout number ex(0)"
 
-
-
 echo "hex encoded data for transaction"
-
 
 $char_array = $data_string.ToCharArray();
 $hex_data = ""
@@ -38,7 +28,6 @@ Foreach ($element in $char_array)
 {
     $hx = [System.String]::Format("{0:x}", [System.Convert]::ToUInt32($element))
     $hex_data+=$hx
-    
 }
 
 write-host $hex_data
@@ -53,6 +42,3 @@ write-host "raw transaction " $raw_tx
 
 write-host "sign transcation in wallet using hex output string"
 write-host "send signed transcation from wallet using signed hex output string"
-
-
-
